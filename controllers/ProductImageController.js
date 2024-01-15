f5ec340347ddfc69f4e232ca6f8859701a64b788
Tanu Tarathikhampor <qewtanu@hotmail.com>
2024-01-15 17:44:35 +0700

    update upload image

--- controllers/ProductImageController.js
+++ controllers/ProductImageController.js
@@ -5,7 +5,62 @@ const service = require('./Service');
 const ProductImageModel = require('../models/ProductImageModel');
 const fileUpload = require('express-fileupload');
 const path = require('node:path');
-const fs = require('fs')
+const fs = require('fs');
+const multer = require('multer');
+// const firebaseConfig = require("../controllers/firebase-config")
+
+// import { initializeApp } from "firebase/app";
+// // const initializeApp = require('firebase/app')
+// const getStorage = require('firebase/storage')
+// const ref = require('firebase/storage')
+// const getDownloadURL = require('firebase/storage')
+// const uploadBytesResumable = require('firebase/storage')
+
+// console.log(firebaseConfig);
+
+
+// initializeApp(firebaseConfig)
+
+
+// const storage = getStorage()
+
+// const upload = multer({ storage: multer.memoryStorage() })
+
+
+// app.post("/testpost", upload.single("filename"), async (req, res) => {
+
+//     try {
+//         const dateTime = giveCurrentDateTime();
+
+//         const storageRef = ref(storage, `image/${req.file.originalname + "       " + dateTime}`);
+
+//         // Create file metadata including the content type
+//         const metadata = {
+//             contentType: req.file.mimetype,
+//         };
+
+//         // Upload the file in the bucket storage
+//         const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+//         //by using uploadBytesResumable we can control the progress of uploading like pause, resume, cancel
+
+//         // Grab the public url
+//         const downloadURL = await getDownloadURL(snapshot.ref);
+
+//         console.log('File successfully uploaded.');
+//         return res.send({
+//             message: 'file uploaded to firebase storage',
+//             name: req.file.originalname,
+//             type: req.file.mimetype,
+//             downloadURL: downloadURL
+//         })
+//     } catch (error) {
+//         return res.status(400).send(error.message)
+//     }
+// });
+
+
+
+
 
 
 app.use(fileUpload());
@@ -30,31 +85,26 @@ app.post('/productImage/insert', service.isLogin, async (req, res) => {
         // console.log(path.extname(productImage.name));
         const newName = nameArray.join("-") + path.extname(productImage.name)
         const uploadPath = __dirname + '/../uploads/' + newName
-        await productImage.mv(uploadPath, async (err) => {
-            if (err) {
-                throw new Error(err)
+        await ProductImageModel.findAll({
+            where: {
+                productId: req.body.productId
             }
-            await ProductImageModel.findAll({
-                where: {
+        }).then(async (res) => {
+            if (res.length === 0) {
+                await ProductImageModel.create({
+                    isMain: true,
+                    imageName: req.body.productImageName,
                     productId: req.body.productId
-                }
-            }).then(async (res) => {
-                if (res.length === 0) {
-                    await ProductImageModel.create({
-                        isMain: true,
-                        imageName: newName,
-                        productId: req.body.productId
-                    })
-                } else {
-                    await ProductImageModel.create({
-                        isMain: false,
-                        imageName: newName,
-                        productId: req.body.productId
-                    })
-                }
-            })
-            res.send({ message: "success" })
+                })
+            } else {
+                await ProductImageModel.create({
+                    isMain: false,
+                    imageName: req.body.productImageName,
+                    productId: req.body.productId
+                })
+            }
         })
+        res.send({ message: "success" })
 
 
     } catch (error) {
@@ -86,11 +136,6 @@ app.delete('/productImage/delete/:id', service.isLogin, async (req, res) => {
             where: {
                 id: req.params.id
             }
-        })
-
-
-        fs.unlink(imgPath, function (err) {
-            if (err) throw err;
         })
 
         res.send({ message: "success" })
